382e4cd73fccce29092d5f4cbd04ab5c85bbe2bd
sirglorg <sirglorg@19faf298-26a9-11df-ab0a-9f3f633ae91d>
2010-03-14 14:55:47 +0000

    updated methods of car & customer classes

--- trunk/src/carrental/entities/Customer.java
+++ trunk/src/carrental/entities/Customer.java
@@ -14,4 +14,71 @@ public class Customer {
 	private String name;
 	private String surname;
 	//private Adress adress;
+
+	public Customer(int id, String name, String surname) {
+		setId(id);
+		setName(name);
+		setSurname(surname);
+	}
+
+
+	/**
+	 * @return <code>id</code> String
+	 */
+	public int getId() {
+		return id;
+	}
+
+	/**
+	 * sets customers <code>id</code>. Should not be directly changed.
+	 * @param id new unique car id.
+	 * @throws IllegalArgumentException defined id is already in use
+	 */
+	private void setId(int id) {
+		//TODO check for id existence
+		this.id = id;
+	}
+
+	/**
+	 *
+	 * @return <code>name</code> String.
+	 */
+	public String getName() {
+		return name;
+	}
+
+	/**
+	 * sets customers <code>name</code>
+	 * @param name not null String
+	 * @throws IllegalArgumentException
+	 */
+	public void setName(String name) throws IllegalArgumentException {
+		if (name != null) {
+			this.name = name;
+		} else {
+			throw new IllegalArgumentException("String name of the customer can not be set to null.");
+		}
+	}
+
+
+	/**
+	 *
+	 * @return <code>surname</code> String.
+	 */
+	public String getSurname() {
+		return surname;
+	}
+
+	/**
+	 * sets customers <code>surname</code>
+	 * @param surname not null String
+	 * @throws IllegalArgumentException
+	 */
+	public void setSurname(String surname) throws IllegalArgumentException {
+		if (surname != null) {
+			this.surname = surname;
+		} else {
+			throw new IllegalArgumentException("String surname of the customer can not be set to null.");
+		}
+	}
 }
