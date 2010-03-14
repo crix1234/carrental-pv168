ab2e90950f556237c86695e6d738b8875a997b29
sirglorg <sirglorg@19faf298-26a9-11df-ab0a-9f3f633ae91d>
2010-03-14 13:05:05 +0000

    uprava demo prikladu na databaze ze cviceni

--- src/carrental/DatabaseTest.java
+++ src/carrental/DatabaseTest.java
@@ -12,16 +12,18 @@ import java.sql.*;
  * @author S
  */
 public class DatabaseTest {
-    public static void main(String[] args) throws ClassNotFoundException, SQLException {
-        Class.forName("org.apache.derby.jdbc.ClientDriver");
-        Connection c = DriverManager.getConnection("jdbc:derby://localhost:1527/CAR_RENTAL");
-        PreparedStatement st = c.prepareStatement(
-                "SELECT * FROM CUSTOMERS WHERE id = ?");
-        st.setInt(1, 10);
-        ResultSet rs = st.executeQuery();
-        while(rs.next()) {
-            System.out.println("id=" + rs.getInt("id")
-                    +" name=" + rs.getString("name"));
-        }
-    }
+	public static void main(String[] args) throws ClassNotFoundException, SQLException, InstantiationException, IllegalAccessException {
+	String driver = "org.apache.derby.jdbc.ClientDriver";
+		Class.forName(driver).newInstance();
+
+		Connection c = DriverManager.getConnection("jdbc:derby://localhost:1527/CAR_RENTAL","host","host");
+		PreparedStatement st = c.prepareStatement(
+			"SELECT * FROM APP.CUSTOMERS");
+		//st.setInt(1, 1);
+		ResultSet rs = st.executeQuery();
+		while(rs.next()) {
+			System.out.println("id=" + rs.getInt("id")
+				+" name=" + rs.getString("name"));
+		}
+	}
 }
