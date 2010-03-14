dadde42d26b99eef4809f1c946a2195956ccd7e4
sirglorg <sirglorg@19faf298-26a9-11df-ab0a-9f3f633ae91d>
2010-03-14 14:45:58 +0000

    build added to ignore list car and customer classes added

---
+++ src/carrental/entities/Customer.java
@@ -0,0 +1,17 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package carrental.entities;
+
+/**
+ *
+ * @author Pavel Mican
+ */
+public class Customer {
+	private int id;
+	private String name;
+	private String surname;
+	//private Adress adress;
+}
