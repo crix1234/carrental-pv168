99dc7a4ee3c5f2543a6af3727edd683536221f3e
sirglorg <sirglorg@19faf298-26a9-11df-ab0a-9f3f633ae91d>
2010-03-20 00:41:00 +0000

    detaily... uprava class Diagramu proti existujicimu stavu AddresManager definuje deleteAddress (nějak se to opomnělo...)

--- trunk/src/carrental/managers/AddressManager.java
+++ trunk/src/carrental/managers/AddressManager.java
@@ -10,6 +10,7 @@ public interface AddressManager {
 	public Address createNewAddress(int houseNumber, String street, String town, String state, String zipcode) throws AddressManagerException;
 	public Address createNewAddress(Address address) throws AddressManagerException;
 	public void editAddress(Address address) throws AddressManagerException;
+	public void deleteAddress(Address address) throws AddressManagerException ;
 	public Address findAddressByID(int id) throws AddressManagerException;
 	public Collection<Address> findAllAddresses() throws AddressManagerException;
 }
